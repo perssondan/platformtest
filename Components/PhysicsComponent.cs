7274716df7b26e223bdd09e546bf6b88ae54bc52
Dan Persson <dan.persson@cenito.se>
2021-04-12 07:55:55 +0200

    Added gravity to make the hero fall

--- Components/PhysicsComponent.cs
+++ Components/PhysicsComponent.cs
@@ -1,4 +1,6 @@
-﻿using uwpKarate.GameObjects;
+﻿using System;
+using System.Numerics;
+using uwpKarate.GameObjects;
 
 namespace uwpKarate.Components
 {
@@ -9,10 +11,29 @@ namespace uwpKarate.Components
         public PhysicsComponent(GameObject gameObject)
         {
             _gameObject = gameObject;
+
+            Gravity = new Vector2(0f, 80f);
         }
 
         public void Update(World world, TimeSpan timeSpan)
         {
+            var velocityToAdd = (float)timeSpan.TotalSeconds * Gravity;
+            var isOnGround = world.IsOnGround(_gameObject);
+            if (isOnGround && !IsJumping)
+            {
+                // if we're on ground, don't apply more gravity
+                _gameObject.TransformComponent.Velocity *= Vector2.UnitX;
+            }
+            else
+            {
+                _gameObject.TransformComponent.Velocity += velocityToAdd;
+            }
         }
+
+        public Vector2 Gravity { get; private set; }
+
+        private bool IsJumping => _gameObject.TransformComponent.Velocity.Y < 0f;
+
+        private bool IsFalling => _gameObject.TransformComponent.Velocity.Y > 0f;
     }
 }
