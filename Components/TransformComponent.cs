5419d289dafb0c75f0653d5de14e4776b980baa6
Dan Persson <dan.persson@cenito.se>
2021-04-12 07:53:19 +0200

    Changed to use Vector2 instead of separate values for positon and velocity

--- Components/TransformComponent.cs
+++ Components/TransformComponent.cs
@@ -1,13 +1,39 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
+using uwpKarate.GameObjects;
 
 namespace uwpKarate.Components
 {
     public class TransformComponent
     {
-        public int XPos { get; set; }
-        public int YPos { get; set; }
-        public int Velocity { get; set; }
+        private Vector2 _velocity;
+        private Vector2 _maxVelocity = new Vector2(200, 200);
 
-        public Vector2 Vector => new Vector2(XPos, YPos);
+        /// <summary>
+        /// px/sec
+        /// </summary>
+        public Vector2 Velocity
+        {
+            get => _velocity;
+            set
+            {
+                if (value.LengthSquared() > _maxVelocity.LengthSquared())
+                    return;
+
+                _velocity = value;
+            }
+        }
+
+        public Vector2 Position { get; set; }
+
+        public void Update(World world, TimeSpan timeSpan)
+        {
+            var oldPosition = Position;
+            Position += (float)timeSpan.TotalSeconds * Velocity;
+            if (!world.WorldRect.Contains(Position.ToPoint()))
+            {
+                Position = Vector2.Zero;
+            }
+        }
     }
 }
