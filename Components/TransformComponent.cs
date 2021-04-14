4cc02d7b84005b26053d0f5b938f98a9c8ce88b7
Dan Persson <dan.persson@cenito.se>
2021-04-14 17:51:39 +0200

    Reset player position if outside the world

--- Components/TransformComponent.cs
+++ Components/TransformComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Numerics;
 using uwpKarate.GameObjects;
 
@@ -15,12 +16,35 @@ namespace uwpKarate.Components
 
         public void Update(World world, TimeSpan timeSpan)
         {
-            var oldPosition = Position;
             Position += (float)timeSpan.TotalSeconds * Velocity;
             if (!world.WorldRect.Contains(Position.ToPoint()))
             {
-                Position = Vector2.Zero;
+                if (Position.Y < world.WorldRect.Top)//above
+                {
+                    // zero y
+                    Position *= Vector2.UnitX;
+                    Position += (float)world.WorldRect.Top * Vector2.UnitY;
+                }
+                if (Position.Y > world.WorldRect.Bottom)//below
+                {
+                    // zero y
+                    Position *= Vector2.UnitX;
+                    Position += (float)world.WorldRect.Top * Vector2.UnitY;
+                }
+                if (Position.X < world.WorldRect.Left)//left
+                {
+                    // zero x
+                    Position *= Vector2.UnitY;
+                    Position += (float)world.WorldRect.Left * Vector2.UnitX;
+                }
+                if (Position.X > world.WorldRect.Right)//right
+                {
+                    // zero x
+                    Position *= Vector2.UnitY;
+                    Position += (float)world.WorldRect.Right * Vector2.UnitX;
+                }
             }
+            // reset speed each update
             Velocity = Vector2.Zero;
         }
     }
