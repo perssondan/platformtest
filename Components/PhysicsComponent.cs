9bc6eb038562482c701105e98eb5bbe5a931a534
Dan Persson <dan.persson@cenito.se>
2021-04-15 07:13:57 +0200

    First attempt on snapping player to ground

--- Components/PhysicsComponent.cs
+++ Components/PhysicsComponent.cs
@@ -7,27 +7,41 @@ namespace uwpKarate.Components
     public class PhysicsComponent
     {
         private readonly GameObject _gameObject;
+        private Vector2 _previousAppliedVelocity = Vector2.Zero;
+        private float _maxVelocity = 200f;
 
         public PhysicsComponent(GameObject gameObject)
         {
             _gameObject = gameObject;
 
-            Gravity = new Vector2(0f, 250f);
+            Gravity = new Vector2(0f, 56.8f);
         }
 
         public void Update(World world, TimeSpan timeSpan)
         {
-            var velocityToAdd = (float)timeSpan.TotalSeconds * Gravity;
-            var isOnGround = world.IsOnGround(_gameObject);
-            if (isOnGround && !IsJumping)
+            // velocity should increase over time when gravity is applied (no impulse)
+            var velocityToAdd = _previousAppliedVelocity + (float)timeSpan.TotalSeconds * Gravity;
+            var velocity = velocityToAdd.Length();
+            if (velocity > _maxVelocity)
             {
-                // if we're on ground, don't apply more gravity
-                _gameObject.TransformComponent.Velocity *= Vector2.UnitX;
+                var factor = _maxVelocity / velocity;
+                velocityToAdd *= factor;
             }
-            else
+            // snap to ground
+            if (world.TryGetGroundedTile(_gameObject, out var tileGameObject))
             {
-                _gameObject.TransformComponent.Velocity += velocityToAdd;
+                _gameObject.TransformComponent.Position *= Vector2.UnitX;
+                _gameObject.TransformComponent.Position += (Vector2.UnitY * tileGameObject.TransformComponent.Position) - (32f * Vector2.UnitY);
+                velocityToAdd = Vector2.Zero;
             }
+
+            //if (!IsJumping)
+            //{
+            //    velocityToAdd = Vector2.Zero;
+            //}
+
+            _gameObject.TransformComponent.Velocity += velocityToAdd;
+            _previousAppliedVelocity = velocityToAdd;
         }
 
         public Vector2 Gravity { get; private set; }
