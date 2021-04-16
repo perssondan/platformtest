b9250d8c64229cf6b3c7527b718f42c425014ba9
Dan Persson <dan.persson@cenito.se>
2021-04-16 17:06:17 +0200

    Added tracking of positions in HistoryStack

--- Components/TransformComponent.cs
+++ Components/TransformComponent.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using uwpKarate.GameObjects;
+using uwpKarate.Utilities;
 
 namespace uwpKarate.Components
 {
     public class TransformComponent : IGameObjectComponent<World>
     {
-        public Vector2 Velocity { get; set; }
+        private HistoryStack<Vector2> _historyStack = new HistoryStack<Vector2>(100);
+        private Vector2 _position;
 
-        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public Vector2[] PositionHistory => _historyStack.Items.ToArray();
+
+        public Vector2 Position
+        {
+            get => _position;
+            set
+            {
+                if (Velocity.LengthSquared() > 0f)
+                {
+                    _historyStack.Push(value);
+                }
+                
+                _position = value;
+            }
+        }
 
         public void Update(World world, TimeSpan timeSpan)
         {
