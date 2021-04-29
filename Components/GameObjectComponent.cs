62ae3183ed7bb5efcbc16d8674b0de04d6d40e90
Dan Persson <dan.persson@cenito.se>
2021-04-29 22:56:29 +0200

    created a GameObjectComponent base class implementing the interface and stores the gameobject

---
+++ Components/GameObjectComponent.cs
@@ -0,0 +1,14 @@
+﻿using uwpKarate.GameObjects;
+
+namespace uwpKarate.Components
+{
+    public class GameObjectComponent : IGameObjectComponent
+    {
+        public GameObjectComponent(GameObject gameObject)
+        {
+            GameObject = gameObject;
+        }
+
+        protected GameObject GameObject { get; }
+    }
+}
