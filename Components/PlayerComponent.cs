9c7b34281ddc0c9605399b6dca133cc16f9188a0
Dan <dan.persson@cenito.se>
2021-05-10 07:33:04 +0200

    Added PlayerComponent
    To be able to filter out the player

---
+++ Components/PlayerComponent.cs
@@ -0,0 +1,12 @@
+﻿using uwpKarate.GameObjects;
+
+namespace uwpKarate.Components
+{
+    public class PlayerComponent : GameObjectComponent, IGameObjectComponent
+    {
+        public PlayerComponent(GameObject gameObject)
+            : base(gameObject)
+        {
+        }
+    }
+}
