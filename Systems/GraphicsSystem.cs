a4d9cbeba096160c5b28a9de5f011ea31e032dbf
Dan Persson <dan.persson@cenito.se>
2021-05-16 20:40:16 +0200

    Added hookup to the collision event

--- Systems/GraphicsSystem.cs
+++ Systems/GraphicsSystem.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Brushes;
 using Microsoft.Graphics.Canvas.Geometry;
+using Microsoft.Graphics.Canvas.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using uwpKarate.Components;
+using uwpKarate.Events;
 using uwpKarate.Extensions;
 using uwpKarate.GameObjects;
 using Windows.UI;
@@ -15,6 +17,15 @@ namespace uwpKarate.Systems
     public class GraphicsSystem : SystemBase<GraphicsSystem>
     {
         private readonly Dictionary<Color, CanvasSolidColorBrush> _brushes = new Dictionary<Color, CanvasSolidColorBrush>();
+        private readonly List<CollisionInfo> _collisionInfos = new List<CollisionInfo>();
+
+        protected override void Initialize()
+        {
+            EventSystem.Instance.Register<CollisionArgument>(this, collision =>
+            {
+                _collisionInfos.Add(collision.CollisionInfo);
+            });
+        }
 
         public override void Update(World world, TimeSpan deltaTime)
         {
@@ -28,6 +39,8 @@ namespace uwpKarate.Systems
 
             ShapeGraphicsComponentManager.Instance.Components
                 .ForEach(shapeGraphicsComponent => DrawShapeComponent(canvasDrawingSession, deltaTime, shapeGraphicsComponent));
+
+            _collisionInfos.Clear();
         }
 
         private void DrawShapeComponent(CanvasDrawingSession canvasDrawingSession, TimeSpan deltaTime, ShapeGraphicsComponent shapeGraphicsComponent)
@@ -108,19 +121,33 @@ namespace uwpKarate.Systems
             #region Debugging stuff
 
             DrawPositionHistory(canvasDrawingSession, animatedGraphicsComponent.GameObject);
-            DrawRectangleIfColliding(canvasDrawingSession, animatedGraphicsComponent.GameObject);
+            //DrawRectangleIfColliding(canvasDrawingSession, animatedGraphicsComponent.GameObject);
             DrawColliderBoundingBox(canvasDrawingSession, animatedGraphicsComponent.GameObject.ColliderComponent);
-            DrawCollisionPoint(canvasDrawingSession, animatedGraphicsComponent.GameObject.ColliderComponent);
+            //DrawCollisionPoint(canvasDrawingSession, animatedGraphicsComponent.GameObject.ColliderComponent);
             DrawVelocityVector(canvasDrawingSession, animatedGraphicsComponent.GameObject);
+            DrawObjectInfo(canvasDrawingSession, animatedGraphicsComponent.GameObject);
 
             #endregion Debugging stuff
+        }
+
+        private void DrawObjectInfo(CanvasDrawingSession canvasDrawingSession, GameObject gameObject)
+        {
+            canvasDrawingSession.DrawText($"{gameObject.TransformComponent.Position}", gameObject.TransformComponent.Position + new Vector2(0f, 10f), Colors.White, new CanvasTextFormat { FontSize = 6 });
         }
 
         protected void DrawColliderBoundingBox(CanvasDrawingSession canvasDrawingSession, ColliderComponent colliderComponent)
         {
             if (colliderComponent == null) return;
 
-            canvasDrawingSession.DrawRectangle(colliderComponent.BoundingBox, GetCachedBrush(canvasDrawingSession, Colors.Fuchsia));
+            canvasDrawingSession.DrawRectangle(colliderComponent.BoundingBox, GetCachedBrush(canvasDrawingSession, Colors.LightBlue));
+            canvasDrawingSession.FillCircle(colliderComponent.Center, 1.5f, GetCachedBrush(canvasDrawingSession, Colors.LightBlue));
+            //if (colliderComponent.CollisionType == ColliderComponent.CollisionTypes.Static)
+            //{
+            //    var expandedStaticRect = colliderComponent.BoundingBox.Add(new Vector2(32f, 32f));
+            //    canvasDrawingSession.DrawRectangle(expandedStaticRect, GetCachedBrush(canvasDrawingSession, Colors.LightBlue));
+            //}
+
+            DrawCollisionInfos(canvasDrawingSession, _collisionInfos.ToArray());
         }
 
         protected void DrawVelocityVector(CanvasDrawingSession canvasDrawingSession, GameObject gameObject)
@@ -138,6 +165,11 @@ namespace uwpKarate.Systems
             var collisionInfos = colliderComponent?.CollisionInfos;
             if (collisionInfos == null) return;
 
+            DrawCollisionInfos(canvasDrawingSession, collisionInfos);
+        }
+
+        private void DrawCollisionInfos(CanvasDrawingSession canvasDrawingSession, CollisionInfo[] collisionInfos)
+        {
             foreach (var collisionInfo in collisionInfos)
             {
                 canvasDrawingSession.FillCircle(collisionInfo.CollisionPoint,
@@ -145,7 +177,7 @@ namespace uwpKarate.Systems
 
                 canvasDrawingSession.DrawRectangle(collisionInfo.ContactRect, GetCachedBrush(canvasDrawingSession, Colors.Orange));
 
-                var startPoint = collisionInfo.CollisionPoint - (collisionInfo.CollisionNormal * colliderComponent.Size / 2f);
+                var startPoint = collisionInfo.CollisionPoint - (collisionInfo.CollisionNormal * 32f);
                 canvasDrawingSession.DrawLine(startPoint,
                                               startPoint - (collisionInfo.CollisionNormal * 15f),
                                               GetCachedBrush(canvasDrawingSession, Colors.Orange));
@@ -165,16 +197,18 @@ namespace uwpKarate.Systems
 
             var canvasPathBuilder = new CanvasPathBuilder(canvasDrawingSession);
 
-            canvasPathBuilder.BeginFigure(gameObject.TransformComponent.PositionHistory.First());
+            var offset = gameObject.ColliderComponent.Center - gameObject.TransformComponent.Position;
+
+            canvasPathBuilder.BeginFigure(gameObject.TransformComponent.PositionHistory.First() + offset);
             gameObject.TransformComponent.PositionHistory.Skip(1).All(vector =>
             {
-                canvasPathBuilder.AddLine(vector);
+                canvasPathBuilder.AddLine(vector + offset);
                 return true;
             });
 
             canvasPathBuilder.EndFigure(CanvasFigureLoop.Open);
             var pathGeometry = CanvasGeometry.CreatePath(canvasPathBuilder);
-            canvasDrawingSession.DrawGeometry(pathGeometry, GetCachedBrush(canvasDrawingSession, Colors.Red));
+            canvasDrawingSession.DrawGeometry(pathGeometry, GetCachedBrush(canvasDrawingSession, Colors.Red), 1f);
         }
 
         private ICanvasBrush GetCachedBrush(ICanvasResourceCreator canvasResourceCreator, Color colors)
