eb4cc8d03d1dd44dd6921b90d4344ec27c405e29
Dan Persson <dan.persson@cenito.se>
2021-05-16 20:43:07 +0200

    Working more on the particle system

---
+++ Components/ParticleEmitterComponent.cs
@@ -0,0 +1,17 @@
+﻿using uwpKarate.GameObjects;
+
+namespace uwpKarate.Components
+{
+    public class ParticleEmitterComponent : GameObjectComponent, IComponent
+    {
+        public ParticleEmitterComponent(GameObject gameObject) : base(gameObject)
+        {
+            ParticleEmitterComponentManager.Instance.AddComponent(this);
+        }
+
+        protected override void OnDispose()
+        {
+            ParticleEmitterComponentManager.Instance.RemoveComponent(this);
+        }
+    }
+}
---
+++ Components/ParticleEmitterComponentManager.cs
@@ -0,0 +1,11 @@
+﻿namespace uwpKarate.Components
+{
+    public class ParticleEmitterComponentManager : ComponentManager<ParticleEmitterComponent>
+    {
+        protected ParticleEmitterComponentManager()
+        {
+        }
+
+        public static ParticleEmitterComponentManager Instance { get; } = new ParticleEmitterComponentManager();
+    }
+}
