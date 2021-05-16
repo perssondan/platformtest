1ac1db68ca0fc06c3bc6028596d45cce29289f95
Dan Persson <dan.persson@cenito.se>
2021-05-16 20:31:24 +0200

    added id to gameobject

--- GameObjects/GameObject.cs
+++ GameObjects/GameObject.cs
@@ -6,13 +6,17 @@ namespace uwpKarate.GameObjects
 {
     public class GameObject : IDisposable
     {
+        public static int _idCounter = 0;
         private readonly IDictionary<Type, IComponent> _components = new Dictionary<Type, IComponent>();
 
         public GameObject()
         {
+            Id = GenerateId();
             GameObjectManager.AddGameObject(this);
             AddComponent(new TransformComponent(this));
         }
+
+        public int Id { get; }
 
         public void AddComponent<T>(T gameObjectComponent)
             where T : IComponent
@@ -74,5 +78,10 @@ namespace uwpKarate.GameObjects
         public InputComponent InputComponent => GetComponent<InputComponent>();
         public TransformComponent TransformComponent => GetComponent<TransformComponent>();
         public ColliderComponent ColliderComponent => GetComponent<ColliderComponent>();
+
+        private static int GenerateId()
+        {
+            return _idCounter++;
+        }
     }
 }
