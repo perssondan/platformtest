ebdb5bb62aa3e2e0b9b7f3abc19a46a5f75f4e5a
Dan Persson <dan.persson@cenito.se>
2021-05-25 07:48:54 +0200

    Moved color lerp to extension method

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI;
+
+namespace uwpPlatformer.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color Lerp(this Color startColor, Color endColor, float percentage)
+        {
+            var alpha = Lerp(startColor.A, endColor.A, percentage);
+            var red = Lerp(startColor.R, endColor.R, percentage);
+            var green = Lerp(startColor.G, endColor.G, percentage);
+            var blue = Lerp(startColor.B, endColor.B, percentage);
+
+            return Color.FromArgb((byte)alpha, (byte)red, (byte)green, (byte)blue);
+        }
+
+        private static float Lerp(float startValue, float endValue, float by)
+        {
+            return startValue * (1 - by) + (endValue * by);
+        }
+    }
+}
