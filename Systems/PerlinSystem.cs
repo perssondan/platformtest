ae350be2f96092834fe72b65bd256abbb6caeed1
Dan Persson <dan.persson@cenito.se>
2021-06-17 06:56:19 +0200

    removed unused methods from perlin system

--- Systems/PerlinSystem.cs
+++ Systems/PerlinSystem.cs
@@ -28,42 +28,14 @@ namespace uwpPlatformer.Systems
                 .ToArray() // clone
                 .ForEach(result =>
                 {
-                    //UpdateEntityPosition(result.gameObject, result.Item2.Item1, result.Item2.Item2);
-                    //UpdateEntityVelocity(result.gameObject, result.components.Item1, result.components.Item2);
                     UpdateEntityForce(result.gameObject, result.components.Item1, result.components.Item2, result.components.Item3);
                 });
         }
 
-        private void UpdateEntityPosition(GameObject gameObject, PerlinMovementComponent perlinComponent, TransformComponent transform)
-        {
-            var perlinX = _perlinNoise.Noise(new Vector3(perlinComponent.OffsetX, perlinComponent.OffsetX, 0f));
-            perlinComponent.OffsetX += 0.01f;
-            var perlinY = _perlinNoise.Noise(new Vector3(perlinComponent.OffsetY, perlinComponent.OffsetY, 0f));
-            perlinComponent.OffsetY += 0.01f;
-            transform.Position = new Vector2(
-                (float)GameMath.Map((float)perlinX, -1f, 1f, (float)perlinComponent.Bounds.Left, (float)perlinComponent.Bounds.Right),
-                (float)GameMath.Map((float)perlinY, -1f, 1f, (float)perlinComponent.Bounds.Top, (float)perlinComponent.Bounds.Bottom));
-
-
-            // replacing, since I started to use struct as type for my new components...
-            gameObject.AddOrUpdateComponent(perlinComponent);
-        }
-
-        private void UpdateEntityVelocity(GameObject gameObject, PerlinMovementComponent perlinComponent, TransformComponent transform)
-        {
-            var angle = _perlinNoise.Noise(new Vector3(perlinComponent.OffsetX, perlinComponent.OffsetY, 0f)) * (float)Math.PI * 2;
-            var speedVector = angle.VectorFromAngle();
-
-            transform.Velocity = (speedVector * 60);
-
-            perlinComponent.OffsetX += 0.01f;
-            perlinComponent.OffsetY += 0.01f;
-
-            // replacing, since I started to use struct as type for my new components...
-            gameObject.AddOrUpdateComponent(perlinComponent);
-        }
-
-        private void UpdateEntityForce(GameObject gameObject, PerlinMovementComponent perlinComponent, TransformComponent transform, PhysicsComponent physicsComponent)
+        private void UpdateEntityForce(GameObject gameObject,
+                                       PerlinMovementComponent perlinComponent,
+                                       TransformComponent transform,
+                                       PhysicsComponent physicsComponent)
         {
             var deriv = Vector3.Zero;
             var angle = _perlinNoise.Noise(new Vector3(perlinComponent.OffsetX, perlinComponent.OffsetY, 0f)) * (float)Math.PI * 2;
